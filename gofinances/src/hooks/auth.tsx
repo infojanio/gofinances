e8fd4c42d90a1422d4a5db1fc05bd3e4b7af40eb
Janio Florencio dos Santos <info.janio@gmail.com>
2022-09-20 13:05:39 -0300

    login google

--- gofinances/src/hooks/auth.tsx
+++ gofinances/src/hooks/auth.tsx
@@ -1,7 +1,6 @@
 import React, { createContext, ReactNode, useContext, useState } from 'react'
 
 import * as AuthSession from 'expo-auth-session'
-import { UserInfo } from '../screens/Dashboard/styles'
 
 import { LogBox } from 'react-native'
 
@@ -32,19 +31,18 @@ interface AuthorizationResponse {
 
 const AuthContext = createContext({} as IAuthContextData)
 
-//
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User)
-
+  /*
+   */
   //loga com conta da google
   async function signInWithGoogle() {
     try {
       const CLIENT_ID =
-        '686914996065-eg43f54mufblo4rdr0qt40s43db8ap7n.apps.googleusercontent.com'
+        '947512581914-2sqd54jm2lk5u2sbghbi05s1v0edmgds.apps.googleusercontent.com' //id grejaninflorencio
 
       //  '686914996065-eg43f54mufblo4rdr0qt40s43db8ap7n.apps.googleusercontent.com'
-      const REDIRECT_URI =
-        'https://auth.expo.io/@info.janio@gmail.com/gofinance'
+      const REDIRECT_URI = 'https://auth.expo.io/@grejaninflorencio/gofinances'
       const RESPONSE_TYPE = 'token'
       const SCOPE = encodeURI('profile email') //usa-se o encodeURI para que o espaço não quebre a busca
 
@@ -60,7 +58,9 @@ function AuthProvider({ children }: AuthProviderProps) {
         )
 
         const userInfo = await response.json()
+        console.log(userInfo)
 
+        //se deu certo, salva os dados do usuário
         setUser({
           id: userInfo.id,
           email: userInfo.email,
@@ -69,9 +69,9 @@ function AuthProvider({ children }: AuthProviderProps) {
         })
       }
 
-      console.log(UserInfo)
+      //console.log('deu certo')
 
-      AuthSession.startAsync({ authUrl }) //endpoint de autenticação da google
+      //  AuthSession.startAsync({ authUrl }) //endpoint de autenticação da google
     } catch (error) {
       throw new Error(error)
     }
@@ -95,3 +95,101 @@ function useAuth() {
 }
 
 export { AuthProvider, useAuth }
+
+/*
+import React, { createContext, ReactNode, useContext, useState } from 'react'
+
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+
+import { UserInfo } from '../screens/Dashboard/styles'
+
+import { LogBox } from 'react-native'
+
+//web: 313172291784-htrkhibqvv0d6n69abejrpi6q0alrpvf.apps.googleusercontent.com
+//ios: 313172291784-8p1i8cee4o3a2r5bsd0agl2450hea003.apps.googleusercontent.com
+//android: 313172291784-ha7ns55u1cgriq737p54emel0ddv6bfp.apps.googleusercontent.com
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+interface User {
+  id: string
+  name: string
+  email: string
+  photo?: string
+}
+
+interface IAuthContextData {
+  user: User
+  signInWithGoogle(): Promise<void>
+}
+
+interface AuthorizationResponse {
+  params: {
+    access_token: string
+  }
+  type: string
+}
+
+const AuthContext = createContext({} as IAuthContextData)
+
+WebBrowser.maybeCompleteAuthSession()
+
+//
+function AuthProvider({ children }: AuthProviderProps) {
+  const [acessToken, setAcessToken] = React.useState(null)
+  const [user, setUser] = React.useState(null)
+  const [request, response, promtAsunc] = Google.useIdTokenAuthRequest({
+    clientId:
+      '313172291784-htrkhibqvv0d6n69abejrpi6q0alrpvf.apps.googleusercontent.com',
+    iosClientId:
+      '313122971784-8p1i8cee4o3a2r5bsd0agl2450hea003.apps.googleusercontent.com',
+    androidClientId:
+      '313172291784-ha7ns55u1cgriq737p54emel0ddv6bfp.apps.googleusercontent.com',
+  })
+  //const [user, setUser] = useState<User>({} as User)
+
+  //loga com conta da google
+  // async function signInWithGoogle() {
+  //try {
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      setAcessToken(response.authentication?.accessToken)
+      acessToken && fetchUserInfo()
+    }
+  }, [response, acessToken])
+  // } catch (error) {
+  //  throw new Error(error)
+  // }
+
+  async function fetchUserInfo() {
+    let response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+      headers: { Authorization: `Bearer ${acessToken}` },
+    })
+    const useInfo = await response.json()
+    setUser(useInfo)
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signInWithGoogle,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+  return context
+}
+
+export { AuthProvider, useAuth }
+
+*/
